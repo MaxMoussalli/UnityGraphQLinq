57554a5888f5f4ec52ff61836c3d04d2394e3382
Max <jdrff7@gmail.com>
2022-11-14 10:14:07 +0100

    add TestDebug proj for testing new features

---
+++ src/TestDebug/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using GraphQLinq;
+using Newtonsoft.Json;
+
+namespace GraphQLinq.Scaffolding
+{
+    class Program
+    {
+        [Serializable]
+        public class Test
+        {
+            public ID myID { get; set; }
+            public string strID { get; set; }
+        }
+
+        private static void Main(string[] args)
+        {
+            ID id = "CustomId";
+            var tjson = JsonConvert.SerializeObject("CustomId");
+
+            var testData = new Test() { myID = "CustomId", strID = "CustomId" };
+            var complexJson = JsonConvert.SerializeObject(testData);
+            var json = JsonConvert.SerializeObject(id);
+
+
+            var dId = JsonConvert.DeserializeObject<ID>(json);
+            var testDataDeserialized = JsonConvert.DeserializeObject<Test>(complexJson);
+
+            Console.WriteLine("dId: " + dId);
+        }
+    } 
+}
+
+
+
